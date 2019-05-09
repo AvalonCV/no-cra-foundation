2f163ab33a30c27e1d9e5b72c17c0654dbb81c75
AvalonCV <avaloncv@gmail.com>
2019-05-09 22:13:25 +0200

    Work around webpack-type-changes :/

--- src/server/serverRenderer.ts
+++ src/server/serverRenderer.ts
@@ -1,8 +1,45 @@
+import webpack from 'webpack';
 import { Request, Response } from 'express';
 
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { App } from './../app/App';
+
+// Or use 'webpack.compilation.Asset' ?
+type Assets = string | [] | {};
+interface CompilationWithName extends webpack.compilation.Compilation {
+	name?: string;
+}
+
+// This function makes server rendering of asset references consistent with different webpack chunk/entry configurations
+// https://github.com/webpack/webpack-dev-middleware#server-side-rendering
+const normalizeAssets: (assets: Assets) => string[] = (assets: Assets) => {
+	if (typeof assets === 'object' && assets !== null) {
+		return Object.values(assets);
+	} else {
+		return Array.isArray(assets) ? assets : [assets];
+	}
+};
+
+const getWebpackScriptAssets = (res: Response) => {
+	const assets: string[] = [];
+	const webpackStats: webpack.Stats[] = res.locals.webpackStats.stats;
+
+	webpackStats
+		.filter(element => {
+			const { compilation }: { compilation: CompilationWithName } = element;
+			return compilation.name === 'client';
+		})
+		.forEach(element => {
+			for (let asset in element.compilation.assets) {
+				normalizeAssets(asset).forEach(value => {
+					value.endsWith('js') && assets.push(value);
+				});
+			}
+		});
+
+	return assets.map(path => `<script type="text/javascript" src="${path}" defer></script>`).join('\n');
+};
 
 export default function serverRenderer() {
 	return function(_req: Request, res: Response, _next: any) {
@@ -22,7 +59,7 @@ export default function serverRenderer() {
 			</head>
 			<body>
 				<div class="main_root" id="root">${body}</div>
-				<script src="js/client.js" defer="defer"></script>
+				${getWebpackScriptAssets(res)}
 			</body>
 			</html>
 		`;
